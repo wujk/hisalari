75b54e1c469355175ba969f3e918e006c9aafac1
kevin <kevin@careerintlinc.com>
2019-10-18 13:56:43 +0800

    zookeeper 获取配置文件

--- project/core/src/main/java/com/hisalari/properties/PropertiesHelper.java
+++ project/core/src/main/java/com/hisalari/properties/PropertiesHelper.java
@@ -6,32 +6,43 @@ import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.data.Stat;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.core.env.AbstractEnvironment;
 
 import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.CountDownLatch;
 
-public class PropertiesHelper extends org.springframework.beans.factory.config.PropertiesFactoryBean implements ApplicationContextAware{
+public class PropertiesHelper extends org.springframework.beans.factory.config.PropertiesFactoryBean implements ApplicationContextAware {
 
     private ApplicationContext applicationContext;
+
+    @Autowired
+    private AbstractEnvironment environment;
 
     private ZooKeeper zookeeper;
     private CountDownLatch lantch;
 
+    private Properties properties;
+
     @Override
     protected Properties createProperties() throws IOException {
-        Properties p = super.createProperties();
-        if (p == null || p.keySet().size() == 0) {
-            return p;
+        properties = super.createProperties();
+        if (properties == null || properties.keySet().size() == 0) {
+            return properties;
         }
-        System.out.println(p.keySet());
+        System.out.println(properties.keySet());
         try {
             lantch = new CountDownLatch(1);
-            zookeeper = new ZooKeeper(p.getProperty("zookeeper.address"), 10000000, (WatchedEvent event) -> {
+            zookeeper = new ZooKeeper(properties.getProperty("zookeeper.address"), 10000000, (WatchedEvent event) -> {
                 try {
                     if (event.getState() == Watcher.Event.KeeperState.SyncConnected) {
                         logger.info("PropertiesHelper.zookeeper连接成功");
@@ -45,10 +56,10 @@ public class PropertiesHelper extends org.springframework.beans.factory.config.P
                         System.out.println("node:" + node);
                         try {
                             Map<String, Object> pro = JsonMapper.AlwaysMapper().fromJson(node, Map.class);
-                            ReloadBeanProperties reloadBeanProperties = applicationContext.getBean(ReloadBeanProperties.class);
-                            reloadBeanProperties.changeValue(applicationContext, pro);
+                            properties.putAll(pro);
+                            refreshValues();
                         } catch (Exception e) {
-
+                            e.printStackTrace();
                         }
                     }
                     zookeeper.exists("/myconfig", true);
@@ -57,13 +68,47 @@ public class PropertiesHelper extends org.springframework.beans.factory.config.P
                 }
             });
             lantch.await();
-            System.out.println("zookeeper.address:" + p.getProperty("zookeeper.address"));
+            System.out.println("zookeeper.address:" + properties.getProperty("zookeeper.address"));
             String node = getNodeData("/myconfig");
+            Map<String, Object> pro = JsonMapper.AlwaysMapper().fromJson(node, Map.class);
+            properties.putAll(pro);
             System.out.println("node:" + node);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return p;
+        return properties;
+    }
+
+    private void refreshContxt() {
+        AnnotationConfigApplicationContext annotationConfigApplicationContext = (AnnotationConfigApplicationContext)applicationContext;
+        annotationConfigApplicationContext.stop();
+    }
+
+    public void refreshValues() throws Exception {
+       DefaultListableBeanFactory defaultListableBeanFactory = (DefaultListableBeanFactory) applicationContext.getAutowireCapableBeanFactory();
+        String[] names = defaultListableBeanFactory.getBeanDefinitionNames();
+        for (String beanName : names) {
+            System.out.println(beanName+"---------");
+            Object bean = applicationContext.getBean(beanName);
+            Field[] fields = bean.getClass().getDeclaredFields();
+            for (Field field : fields) {
+                Annotation annotation = field.getAnnotation(Value.class);
+                if (annotation != null) {
+                    String value = ((Value) annotation).value();
+                    if (value.startsWith("$")) {
+                        SplExpress express = new SplExpress();
+                        value = express.parseExpress(value);
+                    } else {
+                        SplExpress express = new SplExpress("#{prop['", "']}");
+                        value = express.parseExpress(value);
+                    }
+                    String fieldName = field.getName();
+                    field.setAccessible(true);
+                    field.set(bean, properties.get(value));
+                    field.setAccessible(false);
+                }
+            }
+        }
     }
 
     public String getNodeData(String node) {
