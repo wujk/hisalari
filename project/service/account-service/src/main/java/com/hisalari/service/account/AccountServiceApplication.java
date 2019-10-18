e8e3b051a7e71badbe8a2f0ab0f1c8302542e164
kevin <kevin@careerintlinc.com>
2019-10-18 09:14:07 +0800

    zookeeper 获取配置文件

---
+++ project/service/account-service/src/main/java/com/hisalari/service/account/AccountServiceApplication.java
@@ -0,0 +1,16 @@
+package com.hisalari.service.account;
+
+
+import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication(scanBasePackages = {"com.hisalari"})
+@EnableDubbo
+public class AccountServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AccountServiceApplication.class, args);
+	}
+
+}
